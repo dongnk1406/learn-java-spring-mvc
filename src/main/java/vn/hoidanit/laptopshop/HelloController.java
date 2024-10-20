c6da937225946fd52845ccd4fdc989d27c1055dd
dongnk14 <tryourbest146@gmail.com>
2024-10-21 01:06:07 +0700

    feat: config mysql

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,14 @@ public class HelloController {
     public String index() {
         return "Hello World from Spring Boot!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello World from user page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Hello World from admin page!";
+    }
 }
